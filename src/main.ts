86a054347e847220df73c029885c4fdee1cb507d
nexteditorjs <nexteditorjs@gmail.com>
2022-03-10 19:21:27 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import * as Y from 'yjs';
-import { assert, createEditor, editorBlocks } from '@nexteditorjs/nexteditor-core';
+import { assert, createEditor } from '@nexteditorjs/nexteditor-core';
 import TableBlock from '@nexteditorjs/nexteditor-table-block';
 import { WebsocketProvider } from 'y-websocket';
 import './style.css';
@@ -11,8 +11,6 @@ const yDoc = new Y.Doc();
 // Sync clients with the y-websocket provider
 const websocketProvider = new WebsocketProvider('ws://localhost:4000', 'yjs-demo-2', yDoc);
 
-editorBlocks.registerComplexBlockClass(TableBlock);
-
 const app = document.querySelector<HTMLDivElement>('#app');
 assert(app, 'app does not exists');
 
@@ -22,7 +20,11 @@ yDoc.on('update', (update: any, origin: any, doc: any) => {
   console.log(origin, doc);
   if (!loaded) {
     loaded = true;
-    const editor = createEditor(app, new YjsDoc(yDoc));
+    const editor = createEditor(app, new YjsDoc(yDoc), {
+      components: {
+        blocks: [TableBlock],
+      },
+    });
     (window as any).editor = editor;
   }
 });
